2d3d828dfa93f8886251586d48700ea2dfb85e3e
GarrettSippel <gasi9744@colorado.edu>
2017-03-08 22:28:50 -0700

    First Commit  very base of game is here

---
+++ Final Project/Assets/scripts/ball.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    Vector3 target;
+    Vector3 prevTarget;
+    public Rigidbody rb;
+    Vector3[] lastFive;
+    int fiveC = 0;
+    public float SZP = 0;
+    bool holdAllowed = true;
+    bool released = false;
+
+	void Start () {
+        target = transform.position;
+        prevTarget = transform.position;
+        rb = GetComponent<Rigidbody>();
+        lastFive = new Vector3[5];
+        
+    }
+
+    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
+    }
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "holdarea")
+        {
+            holdAllowed = true;
+        }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "holdarea")
+        {
+            holdAllowed = false;
+        }
+    }
+   
+    void Update()
+    {
+        holdThrowHandler();
+    }
+
+
+    void holdThrowHandler()
+    {
+        if (holdAllowed == true)
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+            {
+                prevTarget = target;
+                target = GetWorldPositionOnPlane(Input.mousePosition, SZP);
+                target.z = SZP;
+                transform.position = target;
+                if (fiveC >= 5)
+                {
+                    fiveC = 0;
+                    lastFive[fiveC] = target - prevTarget;
+                }
+                else
+                {
+                    lastFive[fiveC] = target - prevTarget;
+                    fiveC++;
+                }
+
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                release();
+            }
+
+        }
+        else
+        {
+            if (released == false)
+            {
+                release();
+            }
+        }
+    }
+    void release()
+    {
+        rb.velocity = (calThrow(lastFive)) * 50;
+        released = true;
+    }
+
+    Vector3 calThrow(Vector3[] array)
+    {
+        Vector3 reVec = new Vector3(0,0,0);
+        for(int x=0; x<5; x++)
+        {
+            reVec = reVec + array[x];
+        }
+        reVec.x = reVec.x / 5;
+        reVec.y = reVec.y / 5;
+        reVec.z = SZP;
+        if (reVec.x > 50)
+        {
+            reVec.x = 50;
+        }
+        if (reVec.y > 50)
+        {
+            reVec.y = 50;
+        }
+        if (reVec.x < -50)
+        {
+            reVec.x = -50;
+        }
+        if (reVec.y < -50)
+        {
+            reVec.y = -50;
+        }
+
+        return reVec;
+    }
+ 
+}
---
+++ Final Project/Assets/scripts/cameraBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehavior : MonoBehaviour {
+    Vector3 ballLocation;
+    public GameObject currentBall;
+    float diffX;
+    float diffY;
+    float zoom = -10;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //UpdateZoom();
+        ballLocation = trackBall(currentBall);
+        diffX = transform.position.x - ballLocation.x;
+        diffY = transform.position.y - ballLocation.y;
+        float currentX = transform.position.x;
+        float currentY = transform.position.y;
+        Vector3 posChange = transform.position;
+        Vector3 velocity = Vector3.zero;
+        Vector3 smooth = (Vector3.SmoothDamp(transform.position, ballLocation, ref velocity, .5f));
+        smooth.z = zoom;
+        transform.position = smooth;
+
+    }
+    
+    Vector3 trackBall(GameObject ball)
+    {
+        Vector3 pos = ball.transform.position;
+            return pos;
+    }
+}
