42ac491128cfa9dc32cf8a4541571a17adcde8d6
GarrettSippel <gasi9744@colorado.edu>
2017-03-10 10:34:07 -0700

    Added more camera features
    Camera will not follow character until it is thrown

--- Final Project/Assets/scripts/cameraBehavior.cs
+++ Final Project/Assets/scripts/cameraBehavior.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class cameraBehavior : MonoBehaviour {
     Vector3 ballLocation;
-    public GameObject currentBall;
+    public ball currentBall;
     float diffX;
     float diffY;
     float zoom = -10;
-    
+    bool ballIsReleased;
 
 	// Use this for initialization
 	void Start () {
@@ -19,21 +19,26 @@ public class cameraBehavior : MonoBehaviour {
 	void Update () {
         //UpdateZoom();
         ballLocation = trackBall(currentBall);
-        diffX = transform.position.x - ballLocation.x;
-        diffY = transform.position.y - ballLocation.y;
-        float currentX = transform.position.x;
-        float currentY = transform.position.y;
-        Vector3 posChange = transform.position;
-        Vector3 velocity = Vector3.zero;
-        Vector3 smooth = (Vector3.SmoothDamp(transform.position, ballLocation, ref velocity, .5f));
-        smooth.z = zoom;
-        transform.position = smooth;
+        print(ballIsReleased);
+        if (ballIsReleased == true)
+        {
+            diffX = transform.position.x - ballLocation.x;
+            diffY = transform.position.y - ballLocation.y;
+            float currentX = transform.position.x;
+            float currentY = transform.position.y;
+            Vector3 posChange = transform.position;
+            Vector3 velocity = Vector3.zero;
+            Vector3 smooth = (Vector3.SmoothDamp(transform.position, ballLocation, ref velocity, .1f));
+            smooth.z = zoom;
+            transform.position = smooth;
+        }
 
     }
     
-    Vector3 trackBall(GameObject ball)
+    Vector3 trackBall(ball b)
     {
-        Vector3 pos = ball.transform.position;
+        Vector3 pos = b.transform.position;
+        ballIsReleased = b.getReleased();
             return pos;
     }
 }
