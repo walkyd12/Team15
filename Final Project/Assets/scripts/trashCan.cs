c7bf0673d4dc6ed616d6ef5b7e17b2fa25949885
Walker Schmidt <schmidt.walker@yahoo.com>
2017-04-11 17:40:20 -0600

    New Assets
    Added new assets and prefabs such as trash can, buttons, and a box

---
+++ Final Project/Assets/scripts/trashCan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trashCan : MonoBehaviour
+{
+    bool smashed = false;
+
+    void OnCollisionEnter(Collision c)
+    {
+        print("smashing!");
+        smashed = true;
+    }
+    void OnTriggerStay(Collider other)
+    {
+
+        if (smashed == true)
+        {
+            print("aaaaaa");
+            Vector3 dir = new Vector3(500, -1000, 0);
+            other.GetComponent<Rigidbody>().AddForce(dir, ForceMode.Force);
+        }
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
