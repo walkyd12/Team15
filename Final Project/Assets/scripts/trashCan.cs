06fbde1e4e9c0f0dbb4f54adb63f7850f625818a
GarrettSippel <gasi9744@colorado.edu>
2017-04-20 16:32:19 -0600

    fixing git ignore

--- Final Project/Assets/scripts/trashCan.cs
+++ Final Project/Assets/scripts/trashCan.cs
@@ -6,15 +6,18 @@ public class trashCan : MonoBehaviour
 {
     void OnTriggerStay(Collider other)
     {
-        Vector3 positionOfTrash = gameObject.transform.position;
-        Vector3 positionOfBall = other.transform.position;
-        Vector3 dir = positionOfTrash - positionOfBall;
-        float mag = dir.magnitude;
-        dir = dir.normalized;
-        dir = dir * 2000 * (1/(mag));
-        print(mag);
+        if (other.tag == "ball")
+        {
+            Vector3 positionOfTrash = gameObject.transform.position;
+            Vector3 positionOfBall = other.transform.position;
+            Vector3 dir = positionOfTrash - positionOfBall;
+            float mag = dir.magnitude;
+            dir = dir.normalized;
+            dir = dir * 2000 * (1 / (mag));
+            print(mag);
 
             other.GetComponent<Rigidbody>().AddForce(dir, ForceMode.Force);
+        }
          
     }
     void Start()
