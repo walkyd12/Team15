b4fdfd03d0d0791ada6b8a4b01f0b927b3a55fbb
GarrettSippel <gasi9744@colorado.edu>
2017-04-20 15:39:24 -0600

    Added a boulder

--- Final Project/Assets/scripts/LevelGoal.cs
+++ Final Project/Assets/scripts/LevelGoal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGoal : MonoBehaviour {
+
     bool levelComplete = false;
     int bonesLeft;
 
@@ -11,6 +12,7 @@ public class LevelGoal : MonoBehaviour {
     public int goal = 0;
     // Use this for initialization
     void Start () {
+        
 		if(goal == 0)
         {
             countBones();
