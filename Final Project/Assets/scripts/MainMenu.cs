908c35aa4d895faa763ad8afe4b123ca33fe9816
jawi7463@colorado.edu <2a101010>
2017-04-29 13:07:46 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master' into jimmy2
    # Conflicts:
    #	Final Project/Assembly-CSharp.csproj
    #	Final Project/Assets/scripts/LevelGoal.cs
    #	Final Project/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- Final Project/Assets/scripts/MainMenu.cs
+++ Final Project/Assets/scripts/MainMenu.cs
@@ -1,25 +1,29 @@
-﻿using System.Collections;
+﻿sing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
-
+    public static string LS = "";
 
     public void loadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName, LoadSceneMode.Single);
-        if (PF_stuff.loggedin == 0)
+        if (!PF_stuff.loggedin)
             PF_stuff.login("Hello");
     }
 
     public void leaderBoard(string string_name)
     {
         PF_stuff.getleaderboard("hello");
-        print(PF_stuff.LB);
-
+        LS = "Rank    Player ID    Points";
+        for (int i = 0; i < 10; i++)
+        {
+            LS = LS + PF_stuff.LB[i].Position.ToString() + " " + PF_stuff.LB[i].PlayFabId.ToString() + " " + PF_stuff.LB[i].StatValue.ToString() + "\n";
+        }
     }
 
     public void login(string x)
@@ -27,13 +31,15 @@ public class MainMenu : MonoBehaviour {
         PF_stuff.login("hello");
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
