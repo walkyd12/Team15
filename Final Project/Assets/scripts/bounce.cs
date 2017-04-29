78152969edb78c9baf4b421ebb2012795bcea2be
GarrettSippel <gasi9744@colorado.edu>
2017-04-29 00:39:32 -0600

    Reworked camera following system

--- Final Project/Assets/scripts/bounce.cs
+++ Final Project/Assets/scripts/bounce.cs
@@ -16,15 +16,16 @@ public class bounce : MonoBehaviour
         float force = 300;
 
         // If the object we hit is the enemy
-        if (c.gameObject.tag == "bounce")
+        if (c.gameObject.tag == "ball")
         {
+            
             // Calculate Angle Between the collision point and the player
             Vector3 dir = c.contacts[0].point - transform.position;
             // We then get the opposite (-Vector3) and normalize it
             dir = -dir.normalized;
             // And finally we add force in the direction of dir and multiply it by force. 
             // This will push back the player
-            GetComponent<Rigidbody>().AddForce(dir * force);
+            c.gameObject.GetComponent<Rigidbody>().AddForce(dir * force, ForceMode.Impulse);
         }
     }
 }
